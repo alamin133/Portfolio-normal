a9c6cc5d486c01a86b8c9713654d914b2d8369a3
Your Name <you@example.com>
2021-09-21 13:12:36 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image:"./imaages/project11.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./imaages/project22.PNG",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./imaages/project33.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./imaages/project-4.png",
+      link: "https://reactbootcamp.com",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://avatars.githubusercontent.com/u/33397752?v=4",
+      name: "Koushik das",
+      company: "G.IT",
+    },
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://avatars.githubusercontent.com/u/33397752?v=4",
+      name: "Koushik das",
+      company: "Dooble.io",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "MongoDB",
+    "Node",
+    "Tailwind",
+    "Material UI",
+  ];
+  
