1d1b97db3f0b38e18e5128598186a0c064c519c9
Your Name <you@example.com>
2021-09-21 13:20:13 +0600

    edit

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export const projects = [
     },
     {
       title: "React Tracks",
-      subtitle: "React and Python",
+      subtitle: "React and Node",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
       image: "./imaages/project22.PNG",
